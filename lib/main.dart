8935e0f5a2bdcbfd06ed1596ee37388da7c4d51d
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-01 02:07:09 +0800

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/adminLogin_screen.dart';
 import 'screens/products_screen.dart';
+import 'screens/product_screen.dart';
+import 'screens/review_screen.dart';
+import 'screens/newReview_screen.dart';
+import 'screens/chat_screen.dart';
+import 'constants.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,11 +18,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LoginScreen(),
+      theme: ThemeData(
+          primaryColor: kTertiaryColor,
+          accentColor: kSecondaryColor,
+          scaffoldBackgroundColor: kPrimaryColor
+      ),
       routes: {
         'Login' : (context) => LoginScreen(),
         'Sign Up' : (context) => SignUpScreen(),
         'Admin Login' : (context) => AdminLogin(),
         'Products' : (context) => ProductsScreen(),
+        'Product' : (context) => ProductScreen(),
+        'Reviews' : (context) => ReviewScreen(),
+        'New Review' : (context) => NewReviewScreen(),
+        'Chat' :(context) => ChatScreen(),
       },
     );
   }
