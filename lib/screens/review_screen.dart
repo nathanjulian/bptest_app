7a402b4ab249f1a1f6005862e3ee91f717fc95b1
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-01 03:30:30 +0800

    review screen skeleton

--- lib/screens/review_screen.dart
+++ lib/screens/review_screen.dart
@@ -1,8 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:bptest_app/constants.dart';
+import 'package:bptest_app/components/page_button.dart';
 
-class ReviewScreen extends StatelessWidget {
+class ReviewScreen extends StatefulWidget {
+  @override
+  _ReviewScreenState createState() => _ReviewScreenState();
+}
+
+class _ReviewScreenState extends State<ReviewScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {}),
+        title: Text('Butterflies & Petals'),
+        backgroundColor: kSecondaryColor,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text('REVIEWS'),
+            PageButton(press: () {Navigator.pushNamed(context, 'New Review');}, text: 'ADD REVIEW'),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: kSecondaryColor,
+                  border: Border.all(color: Colors.black)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('MrPeePee'),
+                  Text('Floaroma'),
+                  Text('Rating: 5/5'),
+                  Text('blah blah blah'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(icon: Icon(Icons.thumb_up), onPressed: (){}),
+                      Text('6'),
+                      IconButton(icon: Icon(Icons.thumb_down), onPressed: (){}),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: kSecondaryColor,
+                  border: Border.all(color: Colors.black)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('MrPooPee'),
+                  Text('Eterna'),
+                  Text('Rating: 3/5'),
+                  Text('blah blah blah'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(icon: Icon(Icons.thumb_up), onPressed: (){}),
+                      Text('6'),
+                      IconButton(icon: Icon(Icons.thumb_down), onPressed: (){}),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
