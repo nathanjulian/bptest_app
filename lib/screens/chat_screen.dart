fe753a4a08ebc013b67667c6b32ea2e08fd81976
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-01 03:51:48 +0800

    chat screen skeleton

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:bptest_app/constants.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListTile(
+              title: Text('PRODUCTS'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, 'Products');
+              },
+            ),
+            ListTile(
+              title: Text('CHAT'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, 'Chat');
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('Butterflies & Petals'),
+        backgroundColor: kSecondaryColor,
+      ),
+    );
   }
 }
