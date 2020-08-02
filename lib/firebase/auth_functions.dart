b73eaa7d8b334c2afe4452e933c5dd6e92de2f42
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-02 23:16:59 +0800

    auth functions

--- lib/firebase/auth_functions.dart
+++ lib/firebase/auth_functions.dart
@@ -1,3 +1,4 @@
+import 'package:bptest_app/components/users.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -5,11 +6,22 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   //create instance object to use FirebaseAuth methods
 
-  Future loginUsername() async {
+
+  //create user obj based on user id
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user !=null ? User(userID: user.uid) : null;
+  }
+
+  //user auth change stream
+  Stream<User> get userLoginStatus {
+    return _auth.onAuthStateChanged.map( _userFromFirebaseUser);
+  }
+
+  Future logIn() async {
     try{
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }catch(e) {
       print(e.toString());
       return null;
