442e49dc454a71cc7f58614b0e0b51d79fae8c34
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-03 00:43:40 +0800

    changed the login and register pages to forms

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,48 +13,61 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final AuthService _auth = AuthService();
 
+  String userEmail = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 30.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username or E-mail',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 50.0),
-            PageButton(
-              text: 'LOG IN',
-              press: () async {
-                dynamic result = await _auth.logIn();
-                result == null ? print('login error') : print('logged in, ' + result.toString());
-              },
-            ),
-            SizedBox(height: 20.0),
-            PageButton(
-                press: () {
-                  Navigator.pushNamed(context, 'Sign Up');
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username or E-mail',
+                ),
+                onChanged: (val) {
+                  setState(() => userEmail = val);
                 },
-                text: 'SIGN UP'),
-            SizedBox(height: 100.0,),
-            PageButton(
-                press: () {
-                  Navigator.pushNamed(context, 'Admin Login');
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                onChanged: (val) {
+                  setState(() => password = val);
                 },
-                text: 'ADMIN LOGIN'),
-          ],
+              ),
+              SizedBox(height: 50.0),
+              PageButton(
+                text: 'LOG IN',
+                press: () async {
+                  print(userEmail);
+                  print(password);
+//                  dynamic result = await _auth.logIn();
+//                  result == null ? print('login error') : print('logged in, ' + result.toString());
+                },
+              ),
+              SizedBox(height: 20.0),
+              PageButton(
+                  press: () {
+                    Navigator.pushNamed(context, 'Sign Up');
+                  },
+                  text: 'SIGN UP'),
+              SizedBox(height: 100.0,),
+              PageButton(
+                  press: () {
+                    Navigator.pushNamed(context, 'Admin Login');
+                  },
+                  text: 'ADMIN LOGIN'),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bptest_app/firebase/auth_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:bptest_app/components/page_button.dart';
 
@@ -7,48 +8,70 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+
+  String email = '';
+  String username = '';
+  String password = '';
+  String confirmPassword = '';
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 30.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'E-mail',
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'E-mail',
+                ),
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
               ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+                onChanged: (val) {
+                  setState(() => username = val);
+                },
               ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
               ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Confirm Password',
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+                onChanged: (val) {
+                  setState(() => confirmPassword = val);
+                },
               ),
-            ),
-            SizedBox(height: 50.0),
-            PageButton(
-              text: 'CREATE ACCOUNT',
-              press: () {},
-            ),
-          ],
+              SizedBox(height: 50.0),
+              PageButton(
+                text: 'CREATE ACCOUNT',
+                press: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
