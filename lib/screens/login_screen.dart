c957ed58784157454e8b574756b17f217c9bffd6
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-02 23:16:51 +0800

    log in function

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bptest_app/firebase/auth_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bptest_app/components/page_button.dart';
@@ -9,6 +10,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +37,10 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(height: 50.0),
             PageButton(
               text: 'LOG IN',
-              press: () {
-                Navigator.pushNamed(context, 'Products');
+              press: () async {
+                dynamic result = await _auth.logIn();
+                result == null ? print('login error') : print('logged in, ' + result.toString());
+                //Navigator.pushNamed(context, 'Products');
               },
             ),
             SizedBox(height: 20.0),
