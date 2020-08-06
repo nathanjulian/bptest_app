3cd6e454a07cdf17ef8c60f764c35b798523109f
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-06 19:35:11 +0800

    created review class to map database docs

---
+++ lib/components/review_class.dart
@@ -0,0 +1,8 @@
+class Review {
+  final String name;
+  final String product;
+  final String rating;
+  final String comments;
+
+  Review({this.name, this.product, this.rating, this.comments});
+}
