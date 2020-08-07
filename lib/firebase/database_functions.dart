db7770405eccb965c4254072e473ba7a93c9115b
nathanjulian <thangama001@mymail.sim.edu.sg>
2020-08-08 07:18:56 +0800

    added collection reference etc

--- lib/firebase/database_functions.dart
+++ lib/firebase/database_functions.dart
@@ -1,38 +1,30 @@
-import 'dart:io';
-
 import 'package:bptest_app/components/users.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:bptest_app/components/review_class.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-
 
 class DatabaseService {
   final String uid;
   DatabaseService({this.uid});
 
-  //collection reference
+  //review collection reference
   final CollectionReference reviewsCollection =
       Firestore.instance.collection('reviews');
 
-
-
-  Future updateUserReview(
-      String name, String product, String rating, String comments, int votes, String photos) async {
+  Future updateUserReview(String name, String product, String rating,
+      String comments, int votes, String photos) async {
     return await reviewsCollection.document(uid).setData({
       'Name': name,
       'Product': product,
       'Rating': rating,
       'Comments': comments,
-      'Votes' : votes,
-      'Photos' : photos,
+      'Votes': votes,
+      'Photos': photos,
     });
   }
 
   Future updateReviewVotes(int votes) async {
     return await reviewsCollection.document(uid).setData({
-      'Votes' : votes,
+      'Votes': votes,
     });
   }
 
@@ -45,11 +37,11 @@ class DatabaseService {
   List<Review> _reviewListFromSnapshot(QuerySnapshot snapshot) {
     return snapshot.documents.map((doc) {
       return Review(
-          name: doc.data['Name'] ?? '',
-          product: doc.data['Product'] ?? '',
-          rating: doc.data['Rating'] ?? '',
-          comments: doc.data['Comments'] ?? '',
-          votes: doc.data['Votes'] ?? '',
+        name: doc.data['Name'] ?? '',
+        product: doc.data['Product'] ?? '',
+        rating: doc.data['Rating'] ?? '',
+        comments: doc.data['Comments'] ?? '',
+        votes: doc.data['Votes'] ?? '',
         photo: doc.data['Photos'] ?? '',
       );
     }).toList();
@@ -57,23 +49,34 @@ class DatabaseService {
 
   //get user document stream
   Stream<UserData> get userData {
-    return reviewsCollection.document(uid).snapshots().map(_userDataFromSnapshot);
+    return reviewsCollection
+        .document(uid)
+        .snapshots()
+        .map(_userDataFromSnapshot);
   }
 
   //userdata snapshot
   UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
     return UserData(
       uid: uid,
-        name: snapshot.data['Name'],
-        product: snapshot.data['Product'],
-        rating: snapshot.data['Rating'],
-        comments: snapshot.data['Comments'],
-        votes: snapshot.data['Votes'],
-    photos : snapshot.data['Photos'],
+      name: snapshot.data['Name'],
+      product: snapshot.data['Product'],
+      rating: snapshot.data['Rating'],
+      comments: snapshot.data['Comments'],
+      votes: snapshot.data['Votes'],
+      photos: snapshot.data['Photos'],
     );
   }
 
+  //chat messages collection reference
+  final CollectionReference chatMessagesCollection =
+      Firestore.instance.collection('messages');
 
-
-
+  sendMessage(String sender, String text) {
+    chatMessagesCollection.add({
+      'Sender' : sender,
+      'Text' : text,
+      'Time' : DateTime.now(),
+    });
+  }
 }
